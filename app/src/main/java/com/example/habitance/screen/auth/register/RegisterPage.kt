50a9d701037c09add0f596074e7d0d49c592ead2
Edi Ginting <ediginting3421@gmail.com>
2024-11-21 20:48:27 +0700

    Build Function Register

--- app/src/main/java/com/example/habitance/screen/auth/register/RegisterPage.kt
+++ app/src/main/java/com/example/habitance/screen/auth/register/RegisterPage.kt
@@ -1,25 +1,335 @@
 package com.example.habitance.screen.auth.register
 
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
-import com.example.habitance.AuthManager
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.transformations
+import coil3.transform.CircleCropTransformation
+import com.example.habitance.R
+import com.example.habitance.data.User
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RegisterPage(
-    modifier: Modifier = Modifier,
-    authManager: NavHostController,
-    navController: AuthManager
-) {
+fun RegisterScreen(navController: NavController) {
+    val viewModel: RegisterViewModel = viewModel()
+    val firstname by viewModel.firstname.collectAsState()
+    val lastname by viewModel.lastname.collectAsState()
+    val username by viewModel.username.collectAsState()
+    val dateOfBirth by viewModel.dateOfBirth.collectAsState()
+    val gender by viewModel.gender.collectAsState()
+    val showDatePicker by viewModel.showDatePicker.collectAsState()
+    val buttonColor = Color(0xFF211321)
+    val context = LocalContext.current
+    val backgroundColor = Color(0xFF473947)
+    val textFieldColor = Color(0xFF796179)
+    val textColor = Color(0xFF1e1e1e)
+    val  currentUser = FirebaseAuth.getInstance().currentUser
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .background(backgroundColor)
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
-        Text(text = "Home Screen", fontSize = 20.sp)
+        Text(
+            text = stringResource(id = R.string.register),
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        var isViewingProfileImage by remember { mutableStateOf(false) }
+
+
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TextField(
+                modifier = Modifier.weight(1f),
+                value = firstname,
+                shape = MaterialTheme.shapes.medium,
+                onValueChange = { viewModel.updateFirstname(it) },
+                label = { Text(text = "First Name", color = textColor) },
+                colors = TextFieldDefaults.colors().copy(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    cursorColor = Color.White,
+                    focusedContainerColor = textFieldColor,
+                    unfocusedContainerColor = textFieldColor,
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            TextField(
+                modifier = Modifier.weight(1f),
+                value = lastname,
+                shape = MaterialTheme.shapes.medium,
+                onValueChange = { viewModel.updateLastname(it) },
+                label = { Text(text = "Last Name", color = textColor) },
+                colors = TextFieldDefaults.colors().copy(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    cursorColor = Color.White,
+                    focusedContainerColor = textFieldColor,
+                    unfocusedContainerColor = textFieldColor,
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+        }
+
+        TextField(
+            value = username,
+            shape = MaterialTheme.shapes.medium,
+            onValueChange = { viewModel.updateUsername(it) },
+            label = { Text(text = "Username", color = textColor) },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors().copy(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.White,
+                focusedContainerColor = textFieldColor,
+                unfocusedContainerColor = textFieldColor,
+            ),
+            textStyle = LocalTextStyle.current.copy(color = Color.White)
+        )
+
+        val datePickerState = rememberDatePickerState(initialDisplayMode = DisplayMode.Picker)
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+        fun Long.toLocalDate(): LocalDate = Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDate()
+
+        TextField(
+            value = dateOfBirth,
+            shape = MaterialTheme.shapes.medium,
+            onValueChange = { },
+            label = { Text("Date of Birth", color = textColor) },
+            modifier = Modifier.fillMaxWidth().clickable {viewModel.updateShowDatePicker(true)},
+            colors = TextFieldDefaults.colors().copy(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.White,
+                focusedContainerColor = textFieldColor,
+                unfocusedContainerColor = textFieldColor,
+            ),
+            textStyle = LocalTextStyle.current.copy(color = Color.White),
+            readOnly = true,
+            trailingIcon = {
+                IconButton(onClick = {viewModel.updateShowDatePicker(true)}) {
+                    Icon(Icons.Default.DateRange, contentDescription = "nwfnwjfw", tint = Color.White)
+                }
+            }
+        )
+
+        if (showDatePicker) {
+            DatePickerDialog(
+                onDismissRequest = {viewModel.updateShowDatePicker(false)},
+                confirmButton = {
+                    TextButton(onClick = {
+                        datePickerState.selectedDateMillis?.let { millis ->
+                            val formattedDate = millis.toLocalDate().format(formatter)
+                            viewModel.updateDateOfBirth(formattedDate)
+                        }
+                        viewModel.updateShowDatePicker(false)
+                    }) {
+                        Text(text = "Select")
+                    }
+                },
+            ) {
+                DatePicker(
+                    state = datePickerState,
+                    showModeToggle = false // Disable mode toggle to ensure only date picking
+                )
+            }
+        }
+
+//gender
+        var isExpanded by remember { mutableStateOf(false) }
+        val genderOptions = listOf("Male", "Female", "Other")
+        val selectedGender by viewModel.gender.collectAsState()
+
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = it } // Mengatur expanded state saat TextField ditekan
+        ) {
+            TextField(
+                value = selectedGender,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Gender", color = textColor) },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                },
+                colors = TextFieldDefaults.colors().copy(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    cursorColor = Color.White,
+                    focusedContainerColor = textFieldColor,
+                    unfocusedContainerColor = textFieldColor,
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor() // Anchor untuk menu dropdown
+                    .clickable { isExpanded = true } // Membuka dropdown saat TextField ditekan
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = { isExpanded = false } // Menutup menu saat kehilangan fokus
+            ) {
+                genderOptions.forEach { genderOption ->
+                    DropdownMenuItem(
+                        text = { Text(genderOption, color = Color.Black) },
+                        onClick = {
+                            viewModel.updateGender(genderOption) // Menyimpan ke ViewModel
+                            isExpanded = false // Menutup dropdown setelah pilihan dipilih
+                        }
+                    )
+                }
+            }
+        }
+
+
+
+
+        Button(
+            onClick = {
+                val userId = currentUser?.uid
+                if (userId != null) {
+                    saveUserDataToFirestore(
+                        user = User(id = Firebase.auth.currentUser!!.uid,
+                            name = firstname,
+                            email = currentUser.email.toString(),
+                            country = "Indonesia", gender = selectedGender),
+                        context = context
+                    ) { isSuccess ->
+                        if (isSuccess) {
+                            Log.d("FirestoreSave", "Data successfully saved to Firestore")
+                        } else {
+                            Log.e("FirestoreSave", "Failed to save data to Firestore")
+                        }
+            }
+                }
+                      },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+        ) {
+            Text(text = "Register", color = Color.White)
+        }
+
+
+
+        TextButton(
+            onClick = {
+                FirebaseAuth.getInstance().signOut()
+                navController.navigate("login")
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Login", color = Color.White)
+        }
+
     }
 }
+
+
+fun saveUserDataToFirestore(user: User, context: Context, callback: (Boolean) -> Unit) {
+    val firestore = FirebaseFirestore.getInstance()
+    val currentUser = FirebaseAuth.getInstance().currentUser
+
+    currentUser?.reload()?.addOnCompleteListener { reloadTask ->
+        if (reloadTask.isSuccessful) {
+                val userMap = mapOf(
+                    "name" to user.name,
+                    "email" to user.email,
+                    "country" to user.country,
+                    "gender" to user.gender
+                )
+                Log.d("userData", user.toString())
+
+                // Simpan data ke koleksi "users" dengan ID dokumen berdasarkan userId
+                firestore.collection("users").document(user.id)
+                    .set(userMap)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("loginStatus", "Data saved for verified account")
+                            callback(true) // Return true if data saved and verified
+                        } else {
+                            Log.e("RegisterActivity", "Failed to save data: ${task.exception?.message}")
+                            callback(false) // Return false if data saving failed
+                        }
+                    }
+        } else {
+            Log.e("loginStatus", "Failed to reload user data: ${reloadTask.exception?.message}")
+            callback(false) // Return false if reloading user data failed
+        }
+    }
+}
+
+
